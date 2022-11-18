67d5bd5f1dfda11dad53fbee3d97f3d858c656b9
faustocintra <professor@faustocintra.com.br>
2022-11-18 15:18:21 +0000

    (18/11) Autenticação com x-access-token na requisição

--- src/api.js
+++ src/api.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 const api = axios.create({
     // Precisa terminar com /
     baseURL: 'https://agoravai-fausto.onrender.com/',
-    timeout: 5000  
+    timeout: 5000,
+    headers: {
+        'x-access-token': window.sessionStorage.getItem('app-data')
+    }  
 })
 
 export default api
