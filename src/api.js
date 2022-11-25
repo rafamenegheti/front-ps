fc1633ff29e4c44ef420deb5a932f2faece77170
faustocintra <professor@faustocintra.com.br>
2022-11-25 13:29:05 +0000

    (25/11) Modificado axios com withCredentials: true

--- src/api.js
+++ src/api.js
@@ -3,10 +3,12 @@ import axios from 'axios'
 const api = axios.create({
     // Precisa terminar com /
     baseURL: 'https://agoravai-fausto.onrender.com/',
-    timeout: 15000,
-    headers: {
+    timeout: 10000,
+    // Envia os cookies seguros de volta em todas as requisições
+    withCredentials: true
+    /*headers: {
         'x-access-token': window.sessionStorage.getItem('app-data')
-    }  
+    }*/
 })
 
 export default api
