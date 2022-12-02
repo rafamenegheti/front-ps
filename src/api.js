1df08e02a9ff44aec7ab6eeeba030818c11f75d0
Rafael Menegheti <rafaelmenegheti52@gmail.com>
2022-12-02 10:53:09 -0300

    voltano

--- src/api.js
+++ src/api.js
@@ -2,7 +2,6 @@ import ky from 'ky'
 
 const api = ky.extend({
     prefixUrl: 'https://agoravai-rafael.onrender.com/',
-    // prefixUrl: 'http://localhost:3000',
     credentials: 'include',
     timeout: 10000
 })
