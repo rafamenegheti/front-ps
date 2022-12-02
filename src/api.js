44807bb54a209bf00441b13a2408ca31cf6349ad
Rafael Menegheti <rafaelmenegheti52@gmail.com>
2022-12-02 09:44:27 -0300

    first commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import ky from 'ky'
 
 const api = ky.extend({
-    prefixUrl: 'https://agoravai-fausto.onrender.com/',
+    prefixUrl: 'https://agoravai-rafael.onrender.com/',
     credentials: 'include',
     timeout: 10000
 })
